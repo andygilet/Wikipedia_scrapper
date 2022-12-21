cc6fc088d26dc0180c27739bbb7bf08de25d8786
andygilet <56910126+andygilet@users.noreply.github.com>
2022-12-21 14:21:21 +0100

    scrapping the data part

--- scrapper.py
+++ scrapper.py
@@ -1,1 +1,32 @@
+import requests
 
+url = "https://country-leaders.onrender.com/"
+status_url = "status"
+country_url = "countries"
+cookie_url = "cookie"
+leaders_url = "leaders"
+
+leaders = []
+
+req = requests.get(url + status_url)
+if req.status_code == 200:
+    print(f"Request to {url}/{status_url} was succesfull!")
+else:
+    print(f"A problem has occured during the request to {url}/{status_url}!")
+    
+cookies = requests.get(url + cookie_url)
+countries = requests.get(url + country_url, 
+                         cookies= cookies.cookies)
+if countries.status_code == 200:
+    print(countries.json())
+    
+for country in countries.json():
+    cookies = requests.get(url + cookie_url)
+    leaders = requests.get(url + leaders_url,
+                           params= {f"country": {country}},
+                           cookies= cookies.cookies)
+if leaders.status_code == 200:
+    print(leaders.json())
+    print("\n")
+else:
+    print(leaders.status_code)
