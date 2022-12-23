a9242974d5841abc2310c9d778e472e175955766
andygilet <andy-gilet@live.be>
2022-12-23 16:08:25 +0100

    I FOUND THE FUCKING BUG !!!!

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,7 @@ from requests import Session
 import bs4
 import re
 from functools import lru_cache
+from urllib3.exceptions import NewConnectionError
 
 def get_countries(session : Session, 
                   url : str, 
@@ -10,12 +11,8 @@ def get_countries(session : Session,
     try:
         cookie = session.get(url + cookie_url)
         countries = session.get(url + country_url, cookies= cookie.cookies)
-        if countries.status_code == 403:
-            raise Exception("Error : cookie expired while trying to get the countries abreviation !")
-        elif countries.status_code != 200:
-            raise Exception("Error : Unable to acces the countries abreviation !")
-    except Exception as e:
-        print(e)
+    except NewConnectionError as nce:
+        print(nce)
         return []
     return countries.json()
 
@@ -29,15 +26,16 @@ def get_leaders_of_country(session : Session,
         leaders = session.get(url + leaders_url,
                              params= {f"country": {country_abv}},
                              cookies= cookie.cookies)
-        if leaders.status_code == 403:
-            raise Exception(f"Error : Cookie exprired while trying to get the leaders of {country_abv}")
-        elif leaders.status_code != 200:
+        if leaders.status_code != 200:
             raise Exception(f"Error : Unable to get the leader of {country_abv}")
         else:
             return leaders.json()
+    except NewConnectionError as nce:
+        print(nce)
+        return []
     except Exception as e:
         print(e)
-        return []
+        return[]
     
 def get_leaders_of_country_first_paragraph(session : Session, 
                                            leaders : list) -> list:
@@ -47,34 +45,21 @@ def get_leaders_of_country_first_paragraph(session : Session,
                                                            leader['wikipedia_url']))
     return list_of_first_paragraph        
     
-
-@lru_cache(maxsize = None)
+#@lru_cache(maxsize = None)
 def get_first_paragraph(session : Session,
                         url : str):
-    """Get the first paragraph of the wikipedia link given to it"""
-    
-    try:
-        req = session.get(url)
-        if req.status_code == 403:
-            raise Exception("Error : wikipedia cookie expired. AGAIN !!!!")
-        elif req.status_code != 200:
-            raise Exception(f"Error : Unable to access {url}")
-    except Exception as e:
-        print(e)
-        return None
-    
+    """Get the first paragraph of the wikipedia link given to it"""  
+    req = session.get(url)
     soup = bs4.BeautifulSoup(req.text, "html.parser")
     for paragraph in soup.find_all('p'):
-        if paragraph.find_all('b'):
+        for _ in paragraph.find_all('b'):
             #removing parenthese and what is between them
-            first_paragraph = re.sub(" \([^()]+\)", '', paragraph)
+            first_paragraph = re.sub(" \([^()]+\)", '', paragraph.text)
             #removing url link
             first_paragraph = re.sub("(?P<url>https?://[^\s]+)", '', first_paragraph)
-            print(f"First paragraph of {url} found !")
             return first_paragraph
-        else:
-            print(f"Error : First paragraph of {url} NOT found !")
-            return None
+    print(paragraph)
+    return None
         
 def get_leaders():
     """Get all the leaders of the five contries given by the API"""
@@ -104,7 +89,7 @@ def get_leaders():
                                countries[3]: leaders[3],
                                countries[4]: leaders[4]}
         for i in range(len(countries)):
-            first_paragraphs.append(get_leaders_of_country_first_paragraph(session,
+            first_paragraphs.append(get_leaders_of_country_first_paragraph(session, 
                                                                            leaders[i]))
         return leaders_per_country
     
